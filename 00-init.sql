ecc78ae05bca63f9a5a0480acafe5f08af9df130
Max MARIE <max.marie.4@gmail.com>
2023-01-11 15:04:26 +0100

    Update 00-init.sql

--- 00-init.sql
+++ 00-init.sql
@@ -1,4 +1,4 @@
-﻿do
+do
 $$
 begin
 	CREATE TABLE users (
@@ -12,7 +12,7 @@ begin
 	    created_at timestamp not null ,
 	    created_by uuid not null ,
 	    updated_at timestamp not null ,
-	    updated_by uuid not null ,
+	    updated_by uuid not null
 	);
 	ALTER TABLE users ADD CONSTRAINT pk_users PRIMARY KEY ( id );
 	CREATE INDEX idx_users_email ON users ( email );
@@ -20,8 +20,8 @@ begin
 	CREATE TABLE units (
 	    id integer not null ,
 	    name varchar not null ,
-	    stats string not null ,
-		type integer not null ,
+	    stats varchar not null ,
+		type integer not null
 	);
 	ALTER TABLE units ADD CONSTRAINT pk_units PRIMARY KEY ( id );
 
